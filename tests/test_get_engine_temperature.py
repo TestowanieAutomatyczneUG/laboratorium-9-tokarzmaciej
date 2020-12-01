fe94762249f6708629c122e262ffcf60f703838b
Maciej <maciej.tokarz1@wp.pl>
2020-12-01 18:06:41 +0100

    test_temperature_engine_positive

---
+++ tests/test_get_engine_temperature.py
@@ -0,0 +1,18 @@
+from src.sample.car import *
+from unittest.mock import *
+from unittest import TestCase, main
+
+
+class test_Car_getEngineTemperature(TestCase):
+    @patch.object(Car, 'getEngineTemperature')
+    def test_temperature_engine_positive(self, mock_method):
+        # prepare mock
+        mock_method.return_value = "70C temperature engine is ok"
+        # testing
+        test_object = Car()
+        result = test_object.getEngineTemperature()
+        self.assertEqual("70C temperature engine is ok", result)
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_needs_fuel.py
+++ tests/test_needs_fuel.py
@@ -2,23 +2,26 @@ from src.sample.car import *
 from unittest.mock import *
 from unittest import TestCase, main
 
+
 class test_Car_needFuel(TestCase):
     @patch.object(Car, 'needsFuel')
     def test_fuel_method_lack(self, mock_method):
-        #prepare mock
+        # prepare mock
         mock_method.return_value = False
-        #testing
+        # testing
         test_object = Car()
         result = test_object.needsFuel()
         self.assertEqual(False, result)
 
     @patch.object(Car, 'needsFuel')
     def test_fuel_method_full(self, mock_method):
-        #prepare mock
+        # prepare mock
         mock_method.return_value = True
-        #testing
+        # testing
         test_object = Car()
         result = test_object.needsFuel()
         self.assertEqual(True, result)
+
+
 if __name__ == '__main__':
     main()
