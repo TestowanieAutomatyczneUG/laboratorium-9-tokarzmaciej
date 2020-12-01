aad1ae34b7dfc28850ea564d622f13bd8c18f504
Maciej <maciej.tokarz1@wp.pl>
2020-12-01 18:15:42 +0100

    test_temperature_engine_too_low

--- tests/test_get_engine_temperature.py
+++ tests/test_get_engine_temperature.py
@@ -13,15 +13,24 @@ class test_Car_getEngineTemperature(TestCase):
         result = test_object.getEngineTemperature()
         self.assertEqual("70C temperature engine is ok", result)
 
-
     @patch.object(Car, 'getEngineTemperature')
     def test_temperature_engine_too_high(self, mock_method):
-        #prepare mock
+        # prepare mock
         mock_method.return_value = "100C temperature engine is too high"
-        #testing
+        # testing
         test_object = Car()
         result = test_object.getEngineTemperature()
         self.assertEqual("100C temperature engine is too high", result)
+
+    @patch.object(Car, 'getEngineTemperature')
+    def test_temperature_engine_too_low(self, mock_method):
+        # prepare mock
+        mock_method.return_value = "30C temperature engine is too low"
+        # testing
+        test_object = Car()
+        result = test_object.getEngineTemperature()
+        self.assertEqual("30C temperature engine is too low", result)
+
 
 if __name__ == '__main__':
     main()
